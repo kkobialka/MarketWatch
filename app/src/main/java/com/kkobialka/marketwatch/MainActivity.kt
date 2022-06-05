1f0ce616373854057ac754bf851a068472ab7ca1
Krzysztof <kkobialka.k@gmail.com>
2022-06-05 14:40:14 +0200

    Add news feed

--- app/src/main/java/com/kkobialka/marketwatch/MainActivity.kt
+++ app/src/main/java/com/kkobialka/marketwatch/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.kkobialka.marketwatch
 
 import android.annotation.SuppressLint
-import android.icu.text.Transliterator
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.ViewGroup
 import android.view.WindowManager
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.res.ResourcesCompat
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
@@ -19,12 +22,14 @@ import com.anychart.core.cartesian.series.Column
 import com.anychart.enums.Anchor
 import com.anychart.enums.HoverMode
 import com.anychart.enums.TooltipPositionMode
+import com.squareup.picasso.Picasso
 import org.json.JSONException
 
 
 @SuppressLint("StaticFieldLeak")
 private lateinit var textSymbol: TextView
 private lateinit var barChart: AnyChartView
+private lateinit var topLinearLayout: LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,8 +42,108 @@ class MainActivity : AppCompatActivity() {
 
         textSymbol = findViewById(R.id.text_symbol)
         barChart = findViewById(R.id.bar_chart)
+        topLinearLayout = findViewById(R.id.top_layout)
+
+        getNewsFeed()
 
         getStockInfo()
+
+    }
+
+    private fun getNewsFeed() {
+        //API
+
+        val baseUrl = "https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=technology&apikey="
+        val apiKey = Common.ALPHA_API_KEY
+        val url = "$baseUrl$apiKey"
+
+        val request = JsonObjectRequest(Request.Method.GET, url, null, { response ->
+            try {
+
+                if (response != null) {
+
+                    val feed = response.getJSONArray("feed")
+
+                    for (i in 0 until feed.length()){
+                        val news = feed.getJSONObject(i)
+                        val title = news.getString("title")
+                        val summary = news.getString("summary")
+                        val bannerImage = news.getString("banner_image")
+                        val sentimentLevel = news.getString("overall_sentiment_label")
+
+                        //Card layout
+                        val cardLinearLayout = LinearLayout(this)
+
+                        cardLinearLayout.orientation = LinearLayout.VERTICAL
+
+                        val params = RelativeLayout.LayoutParams(
+                            500,
+                            ViewGroup.LayoutParams.MATCH_PARENT
+                        )
+
+                        params.setMargins(40, 40, 20, 40)
+
+                        val cardView = CardView(this)
+                        cardView.radius = 40f
+                        cardView.setCardBackgroundColor(Color.parseColor("#F9A825"))
+                        cardView.layoutParams = params
+                        cardView.cardElevation = 40f
+
+                        val topImage = ImageView(this)
+
+                        val imageParams = RelativeLayout.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            330
+                        )
+
+                        topImage.layoutParams = imageParams
+
+                        if(bannerImage.length > 5){
+                            Picasso.get()
+                                .load(bannerImage)
+                                .resize(300, 150)
+                                .centerInside()
+                                .into(topImage)
+                        }
+
+                        val typeface = ResourcesCompat.getFont(this, R.font.maven_pro)
+
+                        val sentiment = TextView(this)
+                        sentiment.text = sentimentLevel
+                        sentiment.textSize = 10f
+                        sentiment.setTextColor(Color.parseColor("#fc0335"))
+                        sentiment.setTypeface(typeface)
+
+                        val quote = TextView(this)
+                        quote.text = title
+                        quote.textSize = 12f
+                        quote.setTextColor(Color.WHITE)
+                        quote.setTypeface(typeface, Typeface.BOLD)
+
+                        val name = TextView(this)
+                        name.text = summary
+                        name.textSize = 10f
+                        name.setTextColor(Color.parseColor("#E0F2F1"))
+                        name.setTypeface(typeface, Typeface.ITALIC)
+
+                        cardLinearLayout.addView(sentiment)
+                        cardLinearLayout.addView(topImage)
+                        cardLinearLayout.addView(quote)
+                        cardLinearLayout.addView(name)
+                        cardView.addView(cardLinearLayout)
+
+                        topLinearLayout.addView(cardView)
+                    }
+
+                } else {
+                   Toast.makeText(this, "Null", Toast.LENGTH_SHORT).show()
+                }
+
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        }, { error -> error.printStackTrace() })
+        Volley.newRequestQueue(this).add(request)
 
     }
 
@@ -50,7 +155,7 @@ class MainActivity : AppCompatActivity() {
         val request = JsonObjectRequest(Request.Method.GET, url, null, { response ->
             try {
 
-                if(response != null){
+                if (response != null) {
 
                     Toast.makeText(this, "Response is not null", LENGTH_SHORT).show()
 
@@ -58,7 +163,7 @@ class MainActivity : AppCompatActivity() {
 
                     val inflationData = response.getJSONArray("data")
 
-                    for (i in 0 until inflationData.length()){
+                    for (i in 0 until inflationData.length()) {
                         val rate = inflationData.getJSONObject(i)
                         val date = rate.getString("date")
                         val value = rate.getString("value")
@@ -94,7 +199,7 @@ class MainActivity : AppCompatActivity() {
 
                     barChart.setChart(cartesian)
 
-                }else{
+                } else {
                     textSymbol.text = "Null"
                 }
 
